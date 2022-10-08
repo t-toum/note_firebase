b178bf85afa423cd2217e933aa0cd0796ee2074c
toum <nanhtavadh@gmail.com>
2022-10-09 00:30:08 +0700

    authentication value object

---
+++ lib/domain/core/value_object.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'failure.dart';
+
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+  @override
+  bool operator ==(covariant ValueObject other) {
+    if (identical(this, other)) return true;
+
+    return other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'EmailAddress(value: $value)';
+}
